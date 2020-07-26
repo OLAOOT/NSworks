8b242983573bf59e5507a01724e92841873c05b9
shkim787 <shkim061198@gmail.com>
2020-07-26 15:42:09 +0900

    init

--- src/components/sections/TopImage.js
+++ src/components/sections/TopImage.js
@@ -5,7 +5,7 @@ import "react-awesome-slider/dist/captioned.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import styled from "styled-components";
 
-import banner from "./../../img/banner1.png";
+import banner from "./../../img/banner_g.png";
 
 const SlideContainer = styled.div`
   position: absolute;
@@ -16,13 +16,6 @@ const SlideContainer = styled.div`
 `;
 const ImageContainer = styled.div`
   width: 100%;
-<<<<<<< HEAD
-  height: 100%;
-  vertical-align: middle;
-`;
-
-const TextContainer = styled.div`
-=======
   height:100%;
   overflow:hidden;
 `;
@@ -33,7 +26,6 @@ const Image = styled.img`
 `;
 
 const Text_container = styled.div`
->>>>>>> ksh2
   text-align: center;
   position: absolute;
   top: 50%;
@@ -42,32 +34,21 @@ const Text_container = styled.div`
 `;
 
 const Title = styled.div`
-<<<<<<< HEAD
-  font-size: 50px;
-  font-weight: bold;
-  color: #ffffff;
-  text-align: center;
-=======
   font-size:50px;
   font-weight:bold;
   color:#000000;
   text-align:center;
->>>>>>> ksh2
 `;
 
 const TopImage = ({ data }) => {
   return (
     <SlideContainer>
       <ImageContainer>
-<<<<<<< HEAD
-        <Image src={require(data.image)} alt="image" />
-=======
         <Image src={banner} alt="image" />
->>>>>>> ksh2
       </ImageContainer>
-      <TextContainer>
+      <Text_container>
         <Title>{data.title}</Title>
-      </TextContainer>
+      </Text_container>
     </SlideContainer>
   );
 };
